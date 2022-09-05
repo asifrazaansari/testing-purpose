805d7a162a796488223ea969bda271f765408c33
vishalpkale <102423910+vishalpkale@users.noreply.github.com>
2022-09-05 18:21:00 +0530

    route

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,18 +1,13 @@
 const express = require('express');
 const router = express.Router();
-<<<<<<< HEAD
+
+const autherController=require("../controller/authorController")
 const blogController = require('../controller/blogController')
 
 router.post("/blogs", blogController.createBlogs)
-=======
-const autherController=require("../controller/authorController")
-
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
 
 router.post('/authors',autherController.createAuther)
 
->>>>>>> 97c0c65691d40007c8fb78d109ab5b14ec4c2242
+
 
 module.exports = router;
