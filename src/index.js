2909a745a294f56e5be5e656d5bebc0461886744
asifrazaansari <asifraza0231@gmail.com>
2022-09-05 20:08:40 +0530

    modified env and bodyparser removed

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
+require('dotenv').config()
 const express = require('express')
-const bodyParser = require('body-parser')
 const route = require('./routes/route')
 const mongoose = require('mongoose')
 const app = express();
 
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://rahulravidas024:2iYJiS3c54qnkGXA@cluster0.emr6keu.mongodb.net/group66Database", {
+mongoose.connect(process.env.String, {
     useNewUrlParser: true
 })
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
+    .then(() => console.log("MongoDb is connected"))
+    .catch(err => console.log(err))
 
 
 app.use('/', route)
 
-app.listen(process.env.PORT || 3000, function (){
-    console.log('Express app running on port' + (process.env.PORT || 3000))
+app.listen(process.env.PORT, function () {
+    console.log('Express app running on port' + (process.env.PORT))
 })
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const authorController=require("../controller/authorController")
+const authorController = require("../controller/authorController")
 const blogController = require('../controller/blogController')
 
 router.post("/blogs", blogController.createBlogs)
 
-router.post('/authors',authorController.createAuthor)
+router.post('/authors', authorController.createAuthor)
 
 
 module.exports = router;
