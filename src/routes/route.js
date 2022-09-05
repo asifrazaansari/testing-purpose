c96ec39e8ef802bd530e3afba58f26af43a575a9
vishalpkale <102423910+vishalpkale@users.noreply.github.com>
2022-09-05 17:36:19 +0530

    create author

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+const autherController=require("../controller/authorController")
+
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
+
+router.post('/authors',autherController.createAuther)
 
 
 
