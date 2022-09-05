6ecbde0c41f3f6ed8dc2b7058bffe2feea98dda7
rahulravidas024 <rahulravidas024@gmail.com>
2022-09-05 18:16:06 +0530

    blog controller

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,10 +2,6 @@ const express = require('express');
 const router = express.Router();
 const blogController = require('../controller/blogController')
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
-
 router.post("/blogs", blogController.createBlogs)
 
 module.exports = router;
